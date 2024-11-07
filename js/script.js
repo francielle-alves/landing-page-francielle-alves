4edd0aff37827414e3ba1a1f8047644952fe3cdd
francielle-alves <franciellealvessite@gmail.com>
2024-11-07 00:12:27 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const faqItems = document.querySelectorAll(".faq-item");
+
+//     faqItems.forEach(item => {
+//         const question = item.querySelector(".faq-question");
+//         question.addEventListener("click", () => {
+//             item.classList.toggle("active");
+//             const arrow = question.querySelector(".arrow");
+//             if (item.classList.contains("active")) {
+//                 arrow.innerHTML = "&#9650;"; // Seta para cima
+//             } else {
+//                 arrow.innerHTML = "&#9660;"; // Seta para baixo
+//             }
+//         });
+//     });
+// });
